ab9ea4d9e1177234a74bc8b0c2124b7e86735377
신유진 <hvl2483@gepplus.net>
2026-05-26 10:58:55 +0900

    슬라이드 딤드

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 const toggleBtn = document.querySelector('.button__toggle');
 const swiper = new Swiper('.swiper', {
   slidesPerView: 'auto',
@@ -13,4 +12,20 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+
+const globalButton = document.querySelector('.button__dropdown-title');
+const langOption = document.querySelector('.list__lang-option');
+
+globalButton.addEventListener('click', (event) => {
+  event.stopPropagation();
+
+  globalButton.classList.toggle('is-active');
+  langOption.classList.toggle('is-active');
+});
+
+document.addEventListener('click', () => {
+  globalButton.classList.remove('is-active');
+  langOption.classList.remove('is-active');
 });
